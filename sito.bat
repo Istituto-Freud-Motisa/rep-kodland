65d355577f9dfdd05405b1660f22e39686c70829
Istituto-Freud-Motisa <145660660+Istituto-Freud-Motisa@users.noreply.github.com>
2025-02-07 16:51:13 +0100

    Add files via upload

---
+++ sito.bat
@@ -0,0 +1,3 @@
+@echo off
+
+start https://www.canva.com/design/DAGdIGCquqU/NmkGtwuEAR8hPE-PSreoOw/view?utm_content=DAGdIGCquqU&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h7a8c460f91
